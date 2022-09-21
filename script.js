97341bbc0fffe8439040943e358893c69322afdb
ldinski <95621292+ldinski@users.noreply.github.com>
2022-09-21 23:01:34 +0100

    attempting to work this

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const dayEl = document.querySelector(".data");
+let el = document.querySelectorAll(".column");
+let colVal = document.getElementById("columnval");
 
 fetch("./data.json")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+el.addEventListener("mouseenter", function (opacityChanger) {
+  colVal.classList.add("column-value-opacity");
+});
+
+el.addEventListener("mouseleave", function (opacityReverter) {
+  el.classList.remove(".column-value-opacity");
+});
